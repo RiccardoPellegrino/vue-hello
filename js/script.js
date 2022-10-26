eae73bb0ed76b2895402972ce0911942a29ad4af
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-26 14:47:42 +0200

    scaffolding fine esercizio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use-strict"
+
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message:"Hello vue!",
+            img:"./Logo.png"
+        }
+    }
+}).mount('#app');
